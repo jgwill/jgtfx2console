4f9fad7dac5af2c36cbba86a14fdaa104a2e0cae
JGWill <jgi@jgwill.com>
2024-08-18 04:28:06 -0400

    clean

--- setup.py
+++ setup.py
@@ -18,31 +18,6 @@ def read_version():
 version = read_version()
 
 
-
-# INSTALL_REQUIRES = [
-#     'pandas>=0.25.1',
-#     'python-dotenv>=0.19.2',
-#     'jgtutils>=0.1.67',
-#     'tlid'
-
-# ]
-
-# EXTRAS_DEV_LINT = [
-#     "flake8>=3.6.0,<3.7.0",
-#     "isort>=4.3.4,<4.4.0",
-# ]
-
-# EXTRAS_DEV_TEST = [
-#     "coverage",
-#     "pytest>=3.10",
-# ]
-
-# EXTRAS_DEV_DOCS = [
-#     "readme_renderer",
-#     "sphinx",
-#     "sphinx_rtd_theme>=0.4.0",
-# ]
-
 setup(
     name='jgtfx2console',
     version=version,
@@ -54,18 +29,6 @@ setup(
     packages=find_packages(),
     #packages=find_packages(include=['jgtfx2console', 'jgtfx2console.forexconnect', 'jgtfx2console.forexconnect.lib', 'jgtfx2console.forexconnect.lib.windows', 'jgtfx2console.forexconnect.lib.linux','jgtfx2console/**'], exclude=['*test*']),
     include_package_data=True,
-    #install_requires=INSTALL_REQUIRES,
-    # entry_points={
-    #     'console_scripts': ['fxcli2console=jgtfx2console.fxcli2console:main'],
-    # },
-    # extras_require={
-    #     'dev': (EXTRAS_DEV_LINT + EXTRAS_DEV_TEST + EXTRAS_DEV_DOCS),
-    #     'dev-lint': EXTRAS_DEV_LINT,
-    #     'dev-test': EXTRAS_DEV_TEST,
-    #     'dev-docs': EXTRAS_DEV_DOCS,
-    # },
-    #license='MIT',
-    #keywords='data',
     classifiers=[
         "Development Status :: 5 - Production/Stable", 
         "Intended Audience :: Financial and Insurance Industry",
